143d615910066885b136478bd6a2780cccb7b1fc
Harri Hämäläinen <hhamalai@iki.fi>
2013-06-19 16:22:59 +0300

    Ceilometer V2 Web API support

---
+++ ceilometer-client/src/main/java/com/woorea/openstack/ceilometer/v2/api/StatisticsResource.java
@@ -0,0 +1,43 @@
+package com.woorea.openstack.ceilometer.v2.api;
+
+import com.woorea.openstack.base.client.HttpMethod;
+import com.woorea.openstack.base.client.OpenStackClient;
+import com.woorea.openstack.base.client.OpenStackRequest;
+import com.woorea.openstack.ceilometer.QueryFilter;
+import com.woorea.openstack.ceilometer.v2.model.Statistics;
+
+import java.util.Iterator;
+
+public class StatisticsResource {
+    private OpenStackClient client;
+
+    public StatisticsResource(OpenStackClient client) {
+        this.client = client;
+    }
+
+    public List list(String meterId) {
+        return new List(meterId);
+    }
+
+    public List list(String meterType, java.util.List<QueryFilter> qf) {
+        return new List(meterType, qf);
+    }
+
+    public class List extends OpenStackRequest<Statistics[]> {
+
+        public List(String meterId) {
+            super(client, HttpMethod.GET, new StringBuilder("/meters/").append(meterId).append("/statistics/").toString(), null, Statistics[].class);
+        }
+
+        public List(String meterId, java.util.List< QueryFilter > qf) {
+            super(client, HttpMethod.GET, new StringBuilder("/meters/").append(meterId).append("/statistics/").toString(), null, Statistics[].class);
+            Iterator it = qf.iterator();
+            for ( int i = 0; i < qf.size(); i++ ) {
+                this.queryParam("q.field", qf.get(i).getField());
+                this.queryParam("q.op", qf.get(i).getOp());
+                this.queryParam("q.value", qf.get(i).getValue());
+            }
+        }
+    }
+
+}
--- ceilometer-model/src/main/java/com/woorea/openstack/ceilometer/v2/model/Resource.java
+++ ceilometer-model/src/main/java/com/woorea/openstack/ceilometer/v2/model/Resource.java
@@ -1,8 +1,8 @@
 package com.woorea.openstack.ceilometer.v2.model;
 
-import java.util.Map;
-
 import org.codehaus.jackson.annotate.JsonProperty;
+
+import java.util.Map;
 
 public class Resource {
 	//{"resource_id": "23b55841eedd41e99d5f3f32149ca086", "timestamp": "2013-03-03T15:19:00", "project_id": "23b55841eedd41e99d5f3f32149ca086", "user_id": null, "metadata": {}}
@@ -46,5 +46,5 @@ public class Resource {
 				+ ", project=" + project + ", user=" + user + ", metadata="
 				+ metadata + "]";
 	}
-	
+
 }
